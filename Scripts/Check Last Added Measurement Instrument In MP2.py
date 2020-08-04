bc8a74566ca6d8ab7cff75dca6091df02fb0a90b
parapampam <krzyskowp@gmail.com>
2020-08-04 11:25:06 +0200

    rewriting code

--- Scripts/Check Last Added Measurement Instrument In MP2.py
+++ Scripts/Check Last Added Measurement Instrument In MP2.py
@@ -1,4 +1,3 @@
-import sys
 import Databases as Db
 from tkinter import *
 
@@ -6,7 +5,7 @@ window = Tk()
 window.title("Sprawdź kod obiektu ostatniego przyrządu pomiarowego w bazie danych MP2")
 
 myLabel = Label(window, text=("\nKod obiektu ostatnio dodanego przyrządu pomiarowego to: \n\n"
-                              + str(Db.MP2.getLastMeasurementInstrumentInDatabase(Db.MP2())))
+                              + str(Db.MP2().get_last_leasurement_instrument()))
                 + "\n")
 myLabel.pack(padx=20)
 
