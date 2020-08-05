08065ffeeac999cfbc23c454afa114ccf4128727
parapampam <krzyskowp@gmail.com>
2020-08-05 07:09:12 +0200

    correction of typos

--- Scripts/Check Last Added Measurement Instrument In MP2.py
+++ Scripts/Check Last Added Measurement Instrument In MP2.py
@@ -5,7 +5,7 @@ window = Tk()
 window.title("Sprawdź kod obiektu ostatniego przyrządu pomiarowego w bazie danych MP2")
 
 myLabel = Label(window, text=("\nKod obiektu ostatnio dodanego przyrządu pomiarowego to: \n\n"
-                              + str(Db.MP2().get_last_leasurement_instrument()))
+                              + str(Db.MP2().get_last_measurement_instrument()))
                 + "\n")
 myLabel.pack(padx=20)
 
